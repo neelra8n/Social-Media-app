d07f73dce26ce58b6ada73bd0a71bdd4b6546180
Neel Ratn <neelratn.official@gmail.com>
2020-01-24 02:42:56 +0530

    SignUP done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -9,20 +12,25 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 
-function App() {
+
+class App extends Component {
+  render(){
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path ="/" component ={ Landing } />
-          <div className="container">
-          <Route exact path = "/register" component = {Register} />
-          <Route exact path = "/login" component = {Login} />
-          </div>
-     <Footer />
-    </div>
-    </Router>
+    <Provider store ={ store }>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path ="/" component ={ Landing } />
+              <div className="container">
+              <Route exact path = "/register" component = {Register} />
+              <Route exact path = "/login" component = {Login} />
+              </div>
+        <Footer />
+        </div>
+        </Router>
+    </Provider>
   );
 }
+};
 
 export default App;
